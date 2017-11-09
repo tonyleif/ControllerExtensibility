cdb24e72f06482d3ef5a3569722ac56b89abb296
Anthony Leif <tonyleif@hotmail.com>
2017-11-08 18:44:02 -0600

    Page 528

--- ControllerExtensibility/Controllers/RemoteDataController.cs
+++ ControllerExtensibility/Controllers/RemoteDataController.cs
@@ -1,15 +1,23 @@
 ﻿using System.Web.Mvc;
 using ControllerExtensibility.Models;
+using System.Threading.Tasks;
 
 namespace ControllerExtensibility.Controllers
 {
     public class RemoteDataController : Controller
     {
-        public ActionResult Data()
+        public async Task<ActionResult> Data()
         {
-            RemoteService service = new RemoteService();
-            string data = service.GetRemoteData();
+            //RemoteService service = new RemoteService();
+            //string data = service.GetRemoteData();
+            string data = await Task<string>.Factory.StartNew(() => { return new RemoteService().GerRemoteData(); });
             return View((object)data);
+        }
+
+        public async Task<ActionResult> ConsumeAsyncMethod()
+        {
+            string data = await new RemoteService().GetRemoteDataAsync();
+            return View("Data", (object)data);
         }
     }
 }
--- ControllerExtensibility/Models/RemoteService.cs
+++ ControllerExtensibility/Models/RemoteService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading;
+using System.Threading.Tasks;
 
 namespace ControllerExtensibility.Models
 {
@@ -8,6 +9,15 @@ namespace ControllerExtensibility.Models
         {
             Thread.Sleep(2000);
             return "Hello from the other side of the world";
+        }
+        
+        public async Task<string> GetRemoteDataAsync()
+        {
+            return await Task<string>.Factory.StartNew(() =>
+            {
+                Thread.Sleep(2000);
+                return "Hello from the other side of the world";
+            });
         }
     }
 }
